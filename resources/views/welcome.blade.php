00dbc4036ffea9c7cfa6e73318d792b31264cbc9
Francesco Fusiello <ffusiello22@gmail.com.com>
2025-01-12 12:37:32 +0100

    start project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<x-layout>
+
+    <header class="container-fluid p-5 bg-dark text-center text-white">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <h1 class="display-1">
+                    Il mio Blog
+                </h1>
+            </div>
+        </div>
+    </header>
+
+
+
+
+
+
+
+
+
+</x-layout>
