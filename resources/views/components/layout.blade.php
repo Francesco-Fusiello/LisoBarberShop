416f841782e81153d8a572b44f1db6a5ba20e623
Francesco Fusiello <ffusiello22@gmail.com.com>
2025-03-03 22:13:15 +0100

    add picture homepage

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,7 +7,7 @@
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-    <title>Document</title>
+    <title>Liso Barber Shop</title>
 </head>
 <body>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,28 @@
 <x-layout>
+    <div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="{{ asset('public\storage\img\1.jpg') }}" class="d-block w-100 h-100" alt="Image1">
+          </div>
+          <div class="carousel-item">
+            <img src="https://scontent.fcia5-1.fna.fbcdn.net/v/t1.6435-9/60853653_1138667459636992_589051410276941824_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=GQhCxA4FjZIQ7kNvgEWeys4&_nc_oc=AdhlAf2heirkZbUIiAatt6vGrke7MQa-zPND16lKMITShLQ5EOj9nPdcBJrEcIS0zm2FE_lgS-SMuW8YIt6-9MCN&_nc_zt=23&_nc_ht=scontent.fcia5-1.fna&_nc_gid=ABcKY6AVRDx94qL1g2hmfH9&oh=00_AYC1Zp864vbbiiLnZ6w8gRodYuwJxqgnOi1QOcmRJ9UIEA&oe=67ED671F"  class="d-block w-100 h-100 " alt="Image1">
+          </div>
+          <div class="carousel-item">
+            <img src="https://scontent.fcia5-1.fna.fbcdn.net/v/t1.6435-9/60853653_1138667459636992_589051410276941824_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=GQhCxA4FjZIQ7kNvgEWeys4&_nc_oc=AdhlAf2heirkZbUIiAatt6vGrke7MQa-zPND16lKMITShLQ5EOj9nPdcBJrEcIS0zm2FE_lgS-SMuW8YIt6-9MCN&_nc_zt=23&_nc_ht=scontent.fcia5-1.fna&_nc_gid=ABcKY6AVRDx94qL1g2hmfH9&oh=00_AYC1Zp864vbbiiLnZ6w8gRodYuwJxqgnOi1QOcmRJ9UIEA&oe=67ED671F"  class="d-block w-100 h-100" alt="Image1">
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+    
 
-  
+
 
 
 
