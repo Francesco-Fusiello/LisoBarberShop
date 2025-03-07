f4e6793a5db9650a618bf4d5bfbb2d3511a0db74
Francesco Fusiello <ffusiello22@gmail.com.com>
2025-03-07 20:50:46 +0100

    footer and priceList changes

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,73 +1,42 @@
-<!-- Remove the container if you want to extend the Footer to full width. -->
-
-
-    <footer class="bg-light text-center text-white">
-    <!-- Grid container -->
-    <div class="container p-4 pb-0">
-      <!-- Section: Social media -->
-      <section class="mb-4">
-        <!-- Facebook -->
-        <a
-          class="btn btn-primary btn-floating m-1"
-          style="background-color: #3b5998;"
-          href="#!"
-          role="button"
-          ><i class="fa-brands fa-facebook"></i></a>
-  
-        <!-- Twitter -->
-        <a
-          class="btn btn-primary btn-floating m-1"
-          style="background-color: #55acee;"
-          href="#!"
-          role="button"
-          ><i class="fab fa-twitter"></i
-        ></a>
-  
-        <!-- Google -->
-        <a
-          class="btn btn-primary btn-floating m-1"
-          style="background-color: #dd4b39;"
-          href="#!"
-          role="button"
-          ><i class="fab fa-google"></i
-        ></a>
-  
-        <!-- Instagram -->
-        <a
-          class="btn btn-primary btn-floating m-1"
-          style="background-color: #ac2bac;"
-          href="#!"
-          role="button"
-          ><i class="fab fa-instagram"></i
-        ></a>
-  
-        <!-- Linkedin -->
-        <a
-          class="btn btn-primary btn-floating m-1"
-          style="background-color: #0082ca;"
-          href="#!"
-          role="button"
-          ><i class="fab fa-linkedin-in"></i
-        ></a>
-        <!-- Github -->
-        <a
-          class="btn btn-primary btn-floating m-1"
-          style="background-color: #333333;"
-          href="#!"
-          role="button"
-          ><i class="fab fa-github"></i
-        ></a>
-      </section>
-      <!-- Section: Social media -->
-    </div>
-    <!-- Grid container -->
-  
-    <!-- Copyright -->
-    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-      © 2025 Francesco Fusiello
-    </div>
-    <!-- Copyright -->
-  </footer>
-    
- 
-  <!-- End of .container -->
+<footer class="py-4">
+	<div class="container">
+		<div class="row align-items-center py-4">
+			<div class="col-12 col-lg-3 text-center text-lg-start mb-3 mb-lg-0"></div>
+			<div class="col-12 col-lg-6 navbar-expand text-center mb-3 mb-lg-0">
+				<ul class="list-unstyled d-block d-lg-flex justify-content-center">
+					<li class="nav-item">
+						<a class="text-dark text-decoration-none me-lg-3" href="">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="text-dark text-decoration-none me-lg-3" href="">About</a>
+					</li>
+					<li class="nav-item">
+						<a class="text-dark text-decoration-none me-lg-3" href="">Shop</a>
+					</li>
+					<li class="nav-item">
+						<a class="text-dark text-decoration-none me-lg-3" href="">Gallery</a>
+					</li>
+					<li class="nav-item">
+						<a class="text-dark text-decoration-none" href="">Contact</a>
+					</li>
+				</ul>
+			</div>
+			<div class="col-12 col-lg-3 small text-center text-lg-end">
+				
+				<path d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" fill-rule="evenodd"></path></svg></a>
+			</div>
+		</div>
+		<div class="border-top d-lg-none"></div>
+		<div class="text-center py-3">
+			<a class="me-2" href="">
+        <svg class="bi bi-instagram text-primary" fill="currentColor" height="20" viewbox="0 0 448 512" width="20" xmlns="http://www.w3.org/2000/svg">
+          <path d="M194.4 211.7a53.3 53.3 0 1 0 59.3 88.7 53.3 53.3 0 1 0 -59.3-88.7zm142.3-68.4c-5.2-5.2-11.5-9.3-18.4-12c-18.1-7.1-57.6-6.8-83.1-6.5c-4.1 0-7.9 .1-11.2 .1c-3.3 0-7.2 0-11.4-.1c-25.5-.3-64.8-.7-82.9 6.5c-6.9 2.7-13.1 6.8-18.4 12s-9.3 11.5-12 18.4c-7.1 18.1-6.7 57.7-6.5 83.2c0 4.1 .1 7.9 .1 11.1s0 7-.1 11.1c-.2 25.5-.6 65.1 6.5 83.2c2.7 6.9 6.8 13.1 12 18.4s11.5 9.3 18.4 12c18.1 7.1 57.6 6.8 83.1 6.5c4.1 0 7.9-.1 11.2-.1c3.3 0 7.2 0 11.4 .1c25.5 .3 64.8 .7 82.9-6.5c6.9-2.7 13.1-6.8 18.4-12s9.3-11.5 12-18.4c7.2-18 6.8-57.4 6.5-83c0-4.2-.1-8.1-.1-11.4s0-7.1 .1-11.4c.3-25.5 .7-64.9-6.5-83l0 0c-2.7-6.9-6.8-13.1-12-18.4zm-67.1 44.5A82 82 0 1 1 178.4 324.2a82 82 0 1 1 91.1-136.4zm29.2-1.3c-3.1-2.1-5.6-5.1-7.1-8.6s-1.8-7.3-1.1-11.1s2.6-7.1 5.2-9.8s6.1-4.5 9.8-5.2s7.6-.4 11.1 1.1s6.5 3.9 8.6 7s3.2 6.8 3.2 10.6c0 2.5-.5 5-1.4 7.3s-2.4 4.4-4.1 6.2s-3.9 3.2-6.2 4.2s-4.8 1.5-7.3 1.5l0 0c-3.8 0-7.5-1.1-10.6-3.2zM448 96c0-35.3-28.7-64-64-64H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96zM357 389c-18.7 18.7-41.4 24.6-67 25.9c-26.4 1.5-105.6 1.5-132 0c-25.6-1.3-48.3-7.2-67-25.9s-24.6-41.4-25.8-67c-1.5-26.4-1.5-105.6 0-132c1.3-25.6 7.1-48.3 25.8-67s41.5-24.6 67-25.8c26.4-1.5 105.6-1.5 132 0c25.6 1.3 48.3 7.1 67 25.8s24.6 41.4 25.8 67c1.5 26.3 1.5 105.4 0 131.9c-1.3 25.6-7.1 48.3-25.8 67z"/></svg></a> <a class="me-2" href=""><svg class="bi bi-google text-primary" fill="currentColor" height="20" viewbox="0 0 488 512" width="20" xmlns="http://www.w3.org/2000/svg">
+            <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"/></svg></a> <a class="me-2" href=""><svg class="bi bi-facebook text-primary" fill="currentColor" height="20" viewbox="0 0 16 16" width="20" xmlns="http://www.w3.org/2000/svg">
+			<path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"></path></svg></a>
+		</div>
+		<div class="pt-lg-4 pb-4 pb-lg-3 text-center small">
+			<p class="mb-0"><a class="d-block d-lg-inline text-muted mx-lg-1 mb-2 mb-lg-0" href="">Privacy Policy</a> <a class="d-block d-lg-inline text-muted mx-lg-1 mb-2 mb-lg-0" href="">Terms of Service</a> <a class="d-block d-lg-inline text-muted mx-lg-1 mb-2 mb-lg-0" href="">Site Map</a></p>
+			<p class="text-muted mb-0">© 2025 Francesco Fusiello.</p>
+		</div>
+	</div>
+</footer>
--- resources/views/priceList.blade.php
+++ resources/views/priceList.blade.php
@@ -1,4 +1,10 @@
 <x-layout>
+    <header>
+        <div class="container d-flex justify-content-center">
+            <img src="\images\3.jpg" alt="logo">
+        </div>
+    </header>
+    <h1 class="d-flex justify-content-center pt-5" style="color: #FFD700"><em>Listino prezzi</em></h1>
 
 
 
