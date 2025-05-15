c4bdf10a4101fc589bcde178783ef7f8e9d0a630
Francesco Fusiello <ffusiello22@gmail.com>
2025-05-15 20:16:59 +0200

    error page created

---
+++ resources/views/errors/unauthorized.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    <div class="container py-5 text-center">
+        <h1 class="text-danger">Area riservata</h1>
+        <p class="lead">Questa sezione è accessibile solo agli utenti autenticati.</p>
+        <a href="{{ url('/') }}" class="btn btn-primary mt-3">Torna alla Home</a>
+    </div>
+</x-layout>
