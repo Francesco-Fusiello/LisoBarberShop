3fd6347531165d613bfdf6df7d1a499e51a8c382
Francesco Fusiello <ffusiello22@gmail.com>
2025-05-25 12:49:18 +0200

    added translations

---
+++ resources/lang/en/validation.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'required' => 'Questo campo è obbligatorio.',
+    'email' => 'Inserisci un indirizzo email valido.',
+    'max' => [
+        'string' => 'Non può contenere più di :max caratteri.',
+    ],
+    'min' => [
+        'string' => 'Deve contenere almeno :min caratteri.',
+    ],
+    'confirmed' => 'La conferma non corrisponde.',
+    'unique' => 'Questo valore è già stato utilizzato.',
+    'integer' => 'Deve essere un numero intero.',
+    'numeric' => 'Deve essere un valore numerico.',
+    'date' => 'Deve essere una data valida.',
+];
