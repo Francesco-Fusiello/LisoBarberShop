bc13c00b99a6e955ce931db7c9bef5bdcb1f6750
Francesco Fusiello <ffusiello22@gmail.com>
2025-05-31 20:33:49 +0200

    fixed errors

--- resources/views/livewire/product-crud.blade.php
+++ resources/views/livewire/product-crud.blade.php
@@ -73,7 +73,11 @@
         @endforeach
     </div>
 
-    {{ $products->links() }}
+      <div class="container pt-5">
+            <div class="pagination-custom">
+                {{ $products->links() }}
+            </div>    
+        </div>
 
    
     <div class="modal fade @if ($showDeleteModal) show d-block @endif" tabindex="-1" style="@if ($showDeleteModal) display: block; background-color: rgba(0,0,0,0.5); @endif">
