28f7f46f1547a9a4be1f4b6b49f66d7a87d2ebf5
Francesco Fusiello <ffusiello22@gmail.com>
2025-06-09 20:13:32 +0200

    added functions in admin-review component

--- app/Livewire/AdminDashboard.php
+++ app/Livewire/AdminDashboard.php
@@ -2,14 +2,27 @@
 
 namespace App\Livewire;
 
+use App\Models\Review;
 use Livewire\Component;
 use Illuminate\Support\Facades\Auth;
 
 class AdminDashboard extends Component
 {
+   public function toggleApproval($id)
+    {
+        $review = Review::findOrFail($id);
+        $review->is_approved = !$review->is_approved;
+        $review->save();
+    }
+
+    public function deleteReview($id)
+    {
+        Review::findOrFail($id)->delete();
+    }
+
     public function render()
     {
-         
-        return view('livewire.admin-dashboard')->layout('components.layout'); 
+        $reviews = Review::latest()->get();
+        return view('livewire.admin.review-manager', compact('reviews'));
     }
 }
