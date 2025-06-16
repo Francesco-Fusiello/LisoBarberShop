41e4b5e3295a1d7ef39843cea018711e74df3908
Francesco Fusiello <ffusiello22@gmail.com>
2025-06-16 18:58:46 +0200

    create view product-filter

--- resources/views/livewire/product-filter.blade.php
+++ resources/views/livewire/product-filter.blade.php
@@ -1,3 +1,25 @@
 <div>
-    {{-- The Master doesn't talk, he acts. --}}
+    <input 
+        type="text" 
+        class="form-control mb-3" 
+        placeholder="Cerca prodotto..." 
+        wire:model.debounce.300ms="search"
+    >
+
+    @if($products->isEmpty())
+        <p>Nessun prodotto trovato.</p>
+    @else
+        <div class="row">
+            @foreach ($products as $product)
+                <div class="col-md-4 mb-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $product->name }}</h5>
+                           
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    @endif
 </div>
