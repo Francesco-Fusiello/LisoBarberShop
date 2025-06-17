0a15cbcf725b550631767ba15599c4fa0eb6326c
Francesco Fusiello <ffusiello22@gmail.com>
2025-06-17 19:03:11 +0200

    added function productlist

--- app/Livewire/ProductList.php
+++ app/Livewire/ProductList.php
@@ -3,11 +3,33 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Product;
+use Livewire\WithPagination;
 
 class ProductList extends Component
 {
+    use WithPagination;
+
+    public $filteredProductIds = null;
+
+    protected $listeners = ['filteredProducts' => 'applyFilter'];
+
+    public function applyFilter($ids)
+    {
+        $this->filteredProductIds = is_array($ids) && count($ids) > 0 ? $ids : null;
+        $this->resetPage();
+    }
+
     public function render()
     {
-        return view('livewire.product-list');
+        $query = Product::query();
+
+        if ($this->filteredProductIds !== null) {
+            $query->whereIn('id', $this->filteredProductIds);
+        }
+
+        return view('livewire.product-list', [
+            'products' => $query->latest()->paginate(6)
+        ]);
     }
 }
