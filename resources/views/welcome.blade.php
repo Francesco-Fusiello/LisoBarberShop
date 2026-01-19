57b3a820677bb1fa8266ec94370c03de036a849f
Francesco Fusiello <ffusiello22@gmail.com>
2026-01-19 23:23:51 +0100

    fixed time transition mosaic home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -96,7 +96,6 @@
                 </p>
 
                 <div class="mosaic-inner">
-                    <!-- 9 immagini di prova tutte uguali (2.jpg) -->
                     <div class="m-item m1"><img src="/images/2.jpg" alt="salone" class="m-img"></div>
                     <div class="m-item m2"><img src="/images/mosaic/mosaic2.jpeg" alt="salone" class="m-img"></div>
                     <div class="m-item m3"><img src="/images/mosaic/mosaic3.jpeg" alt="salone" class="m-img"></div>
