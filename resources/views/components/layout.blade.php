146aec0776eab7a1d986fd0368fa398496671596
Francesco Fusiello <ffusiello22@gmail.com>
2026-01-19 23:04:56 +0100

    added background image with preload

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,6 +8,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+     <link rel="preload" as="image" href="/images/sfondo3.jpg">
   
 
     <title>Liso Barber Shop</title>
@@ -16,7 +17,7 @@
 
 <x-navbar/>
 
-    <div class="min-vh-100">
+    <div class="min-vh-100"style="background-image: url('/images/sfondo3.jpg'); background-size: cover; background-position: center; background-repeat: no-repeat; background-attachment: fixed;">
         {{$slot}}
         </div>
 
