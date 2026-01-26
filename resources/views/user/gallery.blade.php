36b39c63355e8e19510a6fdfc5d681ae72c22974
Francesco Fusiello <ffusiello22@gmail.com>
2026-01-27 00:17:00 +0100

    added social icons to gallery page

--- resources/views/user/gallery.blade.php
+++ resources/views/user/gallery.blade.php
@@ -1,19 +1,46 @@
 <x-layout>
 
-    <div class="py-5">
+    <div class="py-3">
         <h1 class="text-center" style="font-size: 3.5rem; font-weight: 700; letter-spacing: 2px; color: #ffffff;">
             LA NOSTRA GALLERY
         </h1>
-
     </div>
+
+    <div class="social-section text-center mt-3">
+    <p class="social-text fst-italic text-white mb-2">
+        Seguici sui nostri canali social per rimanere sempre connesso!
+    </p>
+
+    <!-- Icone sempre in fila -->
+    <div class="social-icons d-flex justify-content-center align-items-center gap-3">
+        <a href="https://www.instagram.com/lisobarbershop/" target="_blank" aria-label="Instagram" class="social-icon instagram">
+            <i class="fa-brands fa-instagram"></i>
+        </a>
+
+        <a href="https://www.facebook.com/lisogianfranco?locale=it_IT" target="_blank" aria-label="Facebook" class="social-icon facebook">
+            <i class="fa-brands fa-facebook-f"></i>
+        </a>
+
+        <a href="https://www.tiktok.com/@lisobarbershop?lang=it" target="_blank" aria-label="TikTok" class="social-icon tiktok">
+            <i class="fa-brands fa-tiktok"></i>
+        </a>
+
+        <a href="https://www.google.com/search?q=lisobarbershop" target="_blank" aria-label="Google" class="social-icon google">
+            <i class="fa-brands fa-google"></i>
+        </a>
+    </div>
+</div>
+
+
 
 
     <div class="container">
         <div class="row gallery">
             @foreach ($images as $image)
-                  <div class="col-4 col-md-4 col-lg-3 mb-2">
+                <div class="col-4 col-md-4 col-lg-3 mb-2">
                     <img src="{{ asset($image->image_path) }}"class="img-fluid gallery-img cursor-pointer"
-                        data-image="{{ asset($image->image_path) }}" data-bs-toggle="modal" data-bs-target="#imageModal">
+                        data-image="{{ asset($image->image_path) }}" data-bs-toggle="modal"
+                        data-bs-target="#imageModal">
                 </div>
             @endforeach
         </div>
