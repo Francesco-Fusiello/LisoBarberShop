7656208d015ba3b56712a66f21231912f102e93c
Francesco Fusiello <ffusiello22@gmail.com>
2026-01-27 19:07:56 +0100

    change positions of services, and their ID

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -1,10 +1,10 @@
 <x-layout>
-  {{-- <header style="position: relative; width: 100%; height: 450px; overflow: hidden;">
-    <img src="/images/2.jpg" alt="services" class="header-img"
-         style="width: 100%; height: 100%; object-fit: cover; display: block;">
-
-    <div class="d-flex flex-column justify-content-center align-items-center"
-         style="position: absolute; top: 0; bottom: 0; left: 0; right: 0;
+    {{-- <header style="position: relative; width: 100%; height: 450px; overflow: hidden;">
+        <img src="/images/2.jpg" alt="services" class="header-img"
+        style="width: 100%; height: 100%; object-fit: cover; display: block;">
+        
+        <div class="d-flex flex-column justify-content-center align-items-center"
+        style="position: absolute; top: 0; bottom: 0; left: 0; right: 0;
                 background: rgba(11,46,29,0.4); text-align: center;">
         <h1 class="display-3 fw-bold" style="color: #ffffff; text-shadow: 2px 2px 6px #000;">
             DEDICATO ALL’UOMO
@@ -20,189 +20,207 @@
 
     <section class="py-5" style=" color: white; font-family: 'Lato', sans-serif;">
         <div class="container">
-            <h2 id="taglio-uomo" class="text-center mb-5 display-4" style="color: #fff; font-family: 'Playfair Display', serif;font-weight: 700;">
+            <h2 id="taglio-uomo" class="text-center mb-5 display-4"
+                style="color: #fff; font-family: 'Playfair Display', serif;font-weight: 700;">
                 I Nostri Servizi
             </h2>
 
             <!-- Servizio: Taglio Uomo -->
-          <div class="row align-items-center mb-5" data-aos="fade-up">
-    <!-- Immagine -->
-    <div class="col-md-6 mt-3 mt-md-0">
-        <img src="/images/services/taglioUomo.jpeg" class="img-fluid" id="rasatura-tradizionale" alt="Taglio Uomo">
-    </div>
+            <div class="row align-items-center mb-5" data-aos="fade-up">
+                <!-- Immagine -->
+                <div class="col-md-6 mt-3 mt-md-0">
+                    <img src="/images/services/taglioUomo.jpeg" class="img-fluid" 
+                        alt="Taglio Uomo">
+                </div>
 
-    <!-- Riquadro testo minimal -->
-    <div class="col-md-6 d-flex">
-        <div class="text-box p-4 w-100" style="background-color: #fff;">
-            <h3 class="mb-3 pt-1" style="color: #000; font-family:  'Playfair Display', serif;">Taglio Uomo</h3>
-            <p style="color: #000;">
-                Il nostro servizio di taglio uomo è pensato per valorizzare ogni tipo di volto e stile personale.
-                Attraverso una consulenza personalizzata, i nostri barbieri esperti realizzano tagli classici o moderni,
-                utilizzando tecniche avanzate per garantire precisione e durata nel tempo.
-            </p>
-        </div>
-    </div>
-</div>
+                <!-- Riquadro testo minimal -->
+                <div class="col-md-6 d-flex">
+                    <div class="text-box p-4 w-100" style="background-color: #fff;">
+                        <h3 class="mb-3 pt-1" style="color: #000; font-family:  'Playfair Display', serif;">Taglio Uomo
+                        </h3>
+                        <p style="color: #000;">
+                            Il nostro servizio di taglio uomo è pensato per valorizzare ogni tipo di volto e stile
+                            personale.
+                            Attraverso una consulenza personalizzata, i nostri barbieri esperti realizzano tagli
+                            classici o moderni,
+                            utilizzando tecniche avanzate per garantire precisione e durata nel tempo.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Servizio: Trattamenti Capelli -->
+            <div class="row align-items-center mb-5 flex-md-row-reverse" data-aos="fade-up">
+                <div class="col-md-6 mt-3 mt-md-0">
+                    <img src="/images/services/trattamentiCapelli.jpeg" class="img-fluid" alt="Trattamenti Capelli">
+                </div>
+
+                <div class="col-md-6 d-flex">
+                    <div class="text-box p-4 w-100" style="background-color: #fff;">
+                        <h3 class="mb-3 pt-1" style="color: #000; font-family: 'Playfair Display', serif;" >
+                            Trattamenti Capelli
+                        </h3>
+                        <p style="color: #000;" id="modellatura-barba">
+                            Prenditi cura della salute dei tuoi capelli con i nostri trattamenti specifici.
+                            Dalla ricostruzione alla detossinazione del cuoio capelluto, utilizziamo prodotti
+                            professionali
+                            per rinforzare e rivitalizzare i tuoi capelli, restituendo loro lucentezza e vitalità.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <!-- Servizio: Modellatura Barba -->
+            <div class="row align-items-center mb-5" data-aos="fade-up" >
+                <div class="col-md-6 mt-3 mt-md-0">
+                    <img src="/images/services/modellaturaBarba.JPG" class="img-fluid" alt="Modellatura Barba">
+                </div>
+
+                <div class="col-md-6 d-flex">
+                    <div class="text-box p-4 w-100" style="background-color: #fff;">
+                        <h3 class="mb-3 pt-1" style="color: #000; font-family: 'Playfair Display', serif;" >
+                            Modellatura Barba
+                        </h3>
+                        <p style="color: #000;">
+                            La barba è un elemento distintivo del tuo look. Offriamo un servizio di modellatura barba su
+                            misura,
+                            che include la definizione delle linee, la regolazione della lunghezza e trattamenti
+                            specifici
+                            per mantenere la barba morbida e curata.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <!-- Servizio: Taglio Bambino -->
+            <div class="row align-items-center mb-5 flex-md-row-reverse" data-aos="fade-up">
+                <div class="col-md-6 mt-3 mt-md-0">
+                    <img src="/images/services/taglioBambino.jpeg" class="img-fluid" alt="Taglio Bambino">
+                </div>
+
+                <div class="col-md-6 d-flex">
+                    <div class="text-box p-4 w-100" style="background-color: #fff;">
+                        <h3 class="mb-3 pt-1" style="color: #000; font-family: 'Playfair Display', serif;">
+                            Taglio Bambino
+                        </h3>
+                        <p style="color: #000;">
+                            Anche i più piccoli meritano un taglio alla moda! Il nostro servizio di taglio bambino è
+                            pensato per offrire
+                            un'esperienza piacevole e divertente, con tagli moderni e adatti all'età.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Servizio: Colorazione Capelli -->
+            <div class="row align-items-center mb-5" data-aos="fade-up">
+                <div class="col-md-6 mt-3 mt-md-0">
+                    <img src="/images/services/colorazioneCapelli.PNG" class="img-fluid" alt="Colorazione Capelli" >
+                </div>
+
+                <div class="col-md-6 d-flex">
+                    <div class="text-box p-4 w-100" style="background-color: #fff;">
+                        <h3 class="mb-3 pt-1" style="color: #000; font-family: 'Playfair Display', serif;">
+                            Colorazione Capelli
+                        </h3>
+                        <p style="color: #000;" id="rasatura-tradizionale">
+                            Desideri un cambio di look o coprire i capelli bianchi? La nostra colorazione capelli offre
+                            una vasta gamma di tonalità,
+                            garantendo un risultato naturale e duraturo. Utilizziamo prodotti senza ammoniaca per
+                            rispettare la salute del capello.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
 
 
 
             <!-- Servizio: Rasatura Tradizionale -->
             <div class="row align-items-center mb-5 flex-md-row-reverse" data-aos="fade-up">
-    <!-- Immagine -->
-    <div class="col-md-6 mt-3 mt-md-0">
-        <img src="/images/services/rasaturaTradizionale.jpeg" class="img-fluid" id="modellatura-barba" alt="Rasatura Tradizionale">
-    </div>
+                <!-- Immagine -->
+                <div class="col-md-6 mt-3 mt-md-0">
+                    <img src="/images/services/rasaturaTradizionale.jpeg" class="img-fluid" 
+                        alt="Rasatura Tradizionale">
+                </div>
 
-    <!-- Riquadro testo minimal -->
-    <div class="col-md-6 d-flex">
-        <div class="text-box p-4 w-100" style="background-color: #fff;">
-            <h3 class="mb-3 pt-1" style="color: #000; font-family: 'Playfair Display', serif;">Rasatura Tradizionale</h3>
-            <p style="color: #000;">
-                Riscopri il piacere della rasatura tradizionale con panno caldo. Un rituale che combina relax e cura della pelle,
-                utilizzando prodotti di alta qualità per una rasatura profonda e confortevole. Ideale per chi desidera un momento di benessere autentico.
-            </p>
-        </div>
-    </div>
-</div>
-
-            <!-- Servizio: Modellatura Barba -->
-            <div class="row align-items-center mb-5" data-aos="fade-up">
-    <div class="col-md-6 mt-3 mt-md-0">
-        <img src="/images/services/modellaturaBarba.JPG" class="img-fluid" alt="Modellatura Barba">
-    </div>
-
-    <div class="col-md-6 d-flex">
-        <div class="text-box p-4 w-100" style="background-color: #fff;">
-            <h3 class="mb-3 pt-1" style="color: #000; font-family: 'Playfair Display', serif;">
-                Modellatura Barba
-            </h3>
-            <p style="color: #000;">
-                La barba è un elemento distintivo del tuo look. Offriamo un servizio di modellatura barba su misura,
-                che include la definizione delle linee, la regolazione della lunghezza e trattamenti specifici
-                per mantenere la barba morbida e curata.
-            </p>
-        </div>
-    </div>
-</div>
-
-
-            <!-- Servizio: Trattamenti Capelli -->
-            <div class="row align-items-center mb-5 flex-md-row-reverse" data-aos="fade-up">
-    <div class="col-md-6 mt-3 mt-md-0">
-        <img src="/images/services/trattamentiCapelli.jpeg" class="img-fluid" alt="Trattamenti Capelli">
-    </div>
-
-    <div class="col-md-6 d-flex">
-        <div class="text-box p-4 w-100" style="background-color: #fff;">
-            <h3 class="mb-3 pt-1" style="color: #000; font-family: 'Playfair Display', serif;">
-                Trattamenti Capelli
-            </h3>
-            <p style="color: #000;">
-                Prenditi cura della salute dei tuoi capelli con i nostri trattamenti specifici.
-                Dalla ricostruzione alla detossinazione del cuoio capelluto, utilizziamo prodotti professionali
-                per rinforzare e rivitalizzare i tuoi capelli, restituendo loro lucentezza e vitalità.
-            </p>
-        </div>
-    </div>
-</div>
-
-
-            <!-- Servizio: Colorazione Capelli -->
-          <div class="row align-items-center mb-5" data-aos="fade-up">
-    <div class="col-md-6 mt-3 mt-md-0">
-        <img src="/images/services/colorazioneCapelli.PNG" class="img-fluid" alt="Colorazione Capelli">
-    </div>
-
-    <div class="col-md-6 d-flex">
-        <div class="text-box p-4 w-100" style="background-color: #fff;">
-            <h3 class="mb-3 pt-1" style="color: #000; font-family: 'Playfair Display', serif;">
-                Colorazione Capelli
-            </h3>
-            <p style="color: #000;">
-                Desideri un cambio di look o coprire i capelli bianchi? La nostra colorazione capelli offre una vasta gamma di tonalità,
-                garantendo un risultato naturale e duraturo. Utilizziamo prodotti senza ammoniaca per rispettare la salute del capello.
-            </p>
-        </div>
-    </div>
-</div>
-
-
-            <!-- Servizio: Taglio Bambino -->
-          <div class="row align-items-center mb-5 flex-md-row-reverse" data-aos="fade-up">
-    <div class="col-md-6 mt-3 mt-md-0">
-        <img src="/images/services/taglioBambino.jpeg" class="img-fluid" alt="Taglio Bambino">
-    </div>
-
-    <div class="col-md-6 d-flex">
-        <div class="text-box p-4 w-100" style="background-color: #fff;">
-            <h3 class="mb-3 pt-1" style="color: #000; font-family: 'Playfair Display', serif;">
-                Taglio Bambino
-            </h3>
-            <p style="color: #000;">
-                Anche i più piccoli meritano un taglio alla moda! Il nostro servizio di taglio bambino è pensato per offrire
-                un'esperienza piacevole e divertente, con tagli moderni e adatti all'età.
-            </p>
-        </div>
-    </div>
-</div>
-
-
+                <!-- Riquadro testo minimal -->
+                <div class="col-md-6 d-flex">
+                    <div class="text-box p-4 w-100" style="background-color: #fff;">
+                        <h3 class="mb-3 pt-1" style="color: #000; font-family: 'Playfair Display', serif;">Rasatura
+                            Tradizionale</h3>
+                        <p style="color: #000;">
+                            Riscopri il piacere della rasatura tradizionale con panno caldo. Un rituale che combina
+                            relax e cura della pelle,
+                            utilizzando prodotti di alta qualità per una rasatura profonda e confortevole. Ideale per
+                            chi desidera un momento di benessere autentico.
+                        </p>
+                    </div>
+                </div>
+            </div>
             <!-- Servizio: Styling e Piega Uomo -->
-           <div class="row align-items-center mb-5" data-aos="fade-up">
-    <div class="col-md-6 mt-3 mt-md-0">
-        <img src="/images/services/stylingPiega.jpeg" class="img-fluid" alt="Styling e Piega Uomo">
-    </div>
+            <div class="row align-items-center mb-5" data-aos="fade-up">
+                <div class="col-md-6 mt-3 mt-md-0">
+                    <img src="/images/services/stylingPiega.jpeg" class="img-fluid" alt="Styling e Piega Uomo">
+                </div>
 
-    <div class="col-md-6 d-flex">
-        <div class="text-box p-4 w-100" style="background-color: #fff;">
-            <h3 class="mb-3 pt-1" style="color: #000; font-family: 'Playfair Display', serif;">
-                Styling e Piega Uomo
-            </h3>
-            <p style="color: #000;">
-                Completa il tuo look con il nostro servizio di styling e piega. Utilizziamo prodotti di alta qualità
-                per modellare i tuoi capelli secondo le ultime tendenze.
-            </p>
-        </div>
-    </div>
-</div>
+                <div class="col-md-6 d-flex">
+                    <div class="text-box p-4 w-100" style="background-color: #fff;">
+                        <h3 class="mb-3 pt-1" style="color: #000; font-family: 'Playfair Display', serif;">
+                            Styling e Piega Uomo
+                        </h3>
+                        <p style="color: #000;">
+                            Completa il tuo look con il nostro servizio di styling e piega. Utilizziamo prodotti di alta
+                            qualità
+                            per modellare i tuoi capelli secondo le ultime tendenze.
+                        </p>
+                    </div>
+                </div>
+            </div>
 
 
             <!-- Servizio: Trattamenti Barba -->
             <div class="row align-items-center mb-5 flex-md-row-reverse" data-aos="fade-up">
-    <div class="col-md-6 mt-3 mt-md-0">
-        <img src="/images/services/trattamentiBarba.PNG" class="img-fluid" alt="Trattamenti Barba">
-    </div>
+                <div class="col-md-6 mt-3 mt-md-0">
+                    <img src="/images/services/trattamentiBarba.PNG" class="img-fluid" alt="Trattamenti Barba">
+                </div>
 
-    <div class="col-md-6 d-flex">
-        <div class="text-box p-4 w-100" style="background-color: #fff;">
-            <h3 class="mb-3 pt-1 mb-4" style="color: #000; font-family: 'Playfair Display', serif;">
-                Trattamenti Barba
+                <div class="col-md-6 d-flex">
+                    <div class="text-box p-4 w-100" style="background-color: #fff;">
+                        <h3 class="mb-3 pt-1 mb-4" style="color: #000; font-family: 'Playfair Display', serif;">
+                            Trattamenti Barba
+                        </h3>
+                        <p style="color: #000;">
+                            Offriamo trattamenti mirati per nutrire, idratare e mantenere la tua barba in perfette
+                            condizioni,
+                            prevenendo irritazioni e secchezza della pelle.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <h3 class="text-center mt-4 pt-5 mb-4"
+                style="font-family: 'Playfair Display', serif; color: #fff; font-weight: 700;">
+                La tua esperienza inizia dall’app: prenotazioni rapide, conferme immediate e attenzione al dettaglio.
+                Scaricala ora.
             </h3>
-            <p style="color: #000;">
-                Offriamo trattamenti mirati per nutrire, idratare e mantenere la tua barba in perfette condizioni,
-                prevenendo irritazioni e secchezza della pelle.
-            </p>
-        </div>
-    </div>
-</div>
 
-            <h3 class="text-center mt-4 pt-5 mb-4" style="font-family: 'Playfair Display', serif; color: #fff; font-weight: 700;">
-                La tua esperienza inizia dall’app: prenotazioni rapide, conferme immediate e attenzione al dettaglio. Scaricala ora.
-            </h3>
-
-            <div style="display: flex; justify-content: center; align-items: center; gap: 20px; flex-wrap: wrap; margin-top: 20px;">
+            <div
+                style="display: flex; justify-content: center; align-items: center; gap: 20px; flex-wrap: wrap; margin-top: 20px;">
                 <!-- Badge App Store -->
                 <a href="https://apps.apple.com/it/app/liso-barber-shop/id6502577739" target="_blank">
                     <img src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/it-it?size=250x83"
-                        alt="Scarica su App Store"
-                        style="height: 60px; max-width: 100%;">
+                        alt="Scarica su App Store" style="height: 60px; max-width: 100%;">
                 </a>
 
                 <!-- Badge Google Play -->
                 <a href="https://play.google.com/store/search?q=liso+barber+shop&c=apps&hl=it" target="_blank">
                     <img src="https://play.google.com/intl/en_us/badges/static/images/badges/it_badge_web_generic.png"
-                        alt="Disponibile su Google Play"
-                        style="height: 74.5px; max-width: 100%;">
+                        alt="Disponibile su Google Play" style="height: 74.5px; max-width: 100%;">
                 </a>
             </div>
         </div>
