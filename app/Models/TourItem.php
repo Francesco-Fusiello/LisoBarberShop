3313a8208071ce46310e7ba994fa4db71f3c1c79
Francesco Fusiello <ffusiello22@gmail.com>
2026-05-22 19:23:50 +0200

    added fillable tour items

--- app/Models/TourItem.php
+++ app/Models/TourItem.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class TourItem extends Model
 {
-    //
+     protected $fillable = [
+        'image',
+        'city',
+        'country',
+        'year',
+        'sort_order'
+    ];
 }
