202cd0706743928505699edd1e00356abd5033f1
Francesco Fusiello <ffusiello22@gmail.com>
2026-05-22 19:21:45 +0200

    created model + migration of tour item

---
+++ database/migrations/2026_05_22_171834_create_tour_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+   public function up(): void
+{
+    Schema::create('tour_items', function (Blueprint $table) {
+        $table->id();
+
+        $table->string('image');
+        $table->string('city');
+        $table->string('country');
+        $table->string('year')->nullable();
+        $table->integer('sort_order')->default(0);
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tour_items');
+    }
+};
