ab6af469c674dfdff654103ee2accfb568ff567a
Francesco Fusiello <ffusiello22@gmail.com>
2026-07-30 22:26:58 +0200

    fix: show only Google reviews with text

--- app/Services/GoogleReviewService.php
+++ app/Services/GoogleReviewService.php
@@ -16,10 +16,11 @@ class GoogleReviewService
         $response = Http::get(
             'https://maps.googleapis.com/maps/api/place/details/json',
             [
-                'place_id' => $placeId,
-                'fields'   => 'reviews,rating,user_ratings_total',
-                'language' => 'it',
-                'key'      => $apiKey,
+                'place_id'      => $placeId,
+                'fields'        => 'reviews,rating,user_ratings_total',
+                'language'      => 'it',
+                'reviews_sort'  => 'newest',
+                'key'           => $apiKey,
             ]
         )->json();
 
@@ -46,12 +47,16 @@ class GoogleReviewService
         if (!empty($result['reviews'])) {
             GoogleReview::truncate();
 
-            foreach (array_slice($result['reviews'], 0, 5) as $review) {
+            $reviews = collect($result['reviews'])
+                ->filter(fn($review) => !empty($review['text']))
+                ->take(5);
+
+            foreach ($reviews as $review) {
                 GoogleReview::create([
-                    'review_id'     => hash('sha256', $review['author_name'].$review['time']),
+                    'review_id'     => hash('sha256', $review['author_name'] . $review['time']),
                     'author_name'   => $review['author_name'],
                     'rating'        => $review['rating'],
-                    'text'          => $review['text'] ?? null,
+                    'text'          => $review['text'] ?? 'Ha lasciato una valutazione.',
                     'profile_photo' => $review['profile_photo_url'] ?? null,
                     'relative_time' => $review['relative_time_description'],
                     'from_google'   => true,
